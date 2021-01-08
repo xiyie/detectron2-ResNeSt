f768dbcb37ecfde24b850f983a9a3762356cfaef
xieqingtao <qingtaotse@gamil.com>
2021-01-08 15:24:34 +0800

    添加VOC

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 #python tools/train_net.py --num-gpus 8 --config-file configs/COCO-Detection/faster_rcnn_R_50_FPN_1x.yaml
 
 
-python tools/train_net.py --num-gpus 8 --config-file configs/COCO-Detection/faster_rcnn_ResNeSt_50_FPN_1x.yaml
+#python tools/train_net.py --num-gpus 2 --config-file configs/COCO-Detection/faster_rcnn_ResNeSt_50_FPN_1x.yaml
 
-
+python tools/train_net.py --num-gpus 2 --config-fileconfigs/PascalVOC-Detection/faster_rcnn_R_50_FPN.yaml #在VOC上
 
